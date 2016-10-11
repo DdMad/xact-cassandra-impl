d99fa1086e0009b77127e9c941e0537f26408d36
DdMad <ddmad1030@gmail.com>
2016-10-12 05:16:35 +0800

    Update data loader and update gitignore.

--- src/main/java/DataLoader.java
+++ src/main/java/DataLoader.java
@@ -39,32 +39,104 @@ public class DataLoader {
         dataFileDir = dir;
     }
 
-    public void loadData(Session session) throws FileNotFoundException {
+    public void loadData(Session session) throws IOException {
         for (final File dataFile : new File(dataFileDir).listFiles()) {
             br = new BufferedReader(new FileReader(dataFile));
             String name = dataFile.getName();
 
             if (name.equals(DATA_CUST_HIST)) {
-
+                loadCustHistData(session, br);
             } else if (name.equals(DATA_CUSTOMER)) {
-
+                loadCustomerData(session, br);
             } else if (name.equals(DATA_DISTRICT)) {
-
+                loadDistrictData(session, br);
             } else if (name.equals(DATA_ITEM)) {
-
+                loadItemData(session, br);
             } else if (name.equals(DATA_NEW_ORDER)) {
-
+                loadNewOrderData(session, br);
             } else if (name.equals(DATA_ORDER)) {
-
+                loadOrderData(session, br);
             } else if (name.equals(DATA_ORDER_LINE)) {
-
+                loadOrderLineData(session, br);
             } else if (name.equals(DATA_STOCK)) {
-
+                loadStockData(session, br);
             } else if (name.equals(DATA_WAREHOUSE)) {
-
+                loadWarehouseData(session, br);
             } else {
                 logger.warn("Wrong data file!");
             }
+        }
+    }
+
+    private void loadCustHistData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadCustomerData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadDistrictData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadItemData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadNewOrderData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadOrderData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadOrderLineData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadStockData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
+        }
+    }
+
+    private void loadWarehouseData(Session session, BufferedReader br) throws IOException {
+        String dataLine = br.readLine();
+        while (dataLine != null) {
+            String[] data = dataLine.split(",");
+
         }
     }
 }

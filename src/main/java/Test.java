869a15f798499e71d93b89beaaa48e414ab30447
DdMad <ddmad1030@gmail.com>
2016-10-13 11:43:40 +0800

    Update setup CQL.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Test {
 
-    private static final String SCHEMA_FILE_PATH = "/src/main/resources/setup.cql";
+    private static final String SCHEMA_FILE_PATH = "/src/main/resources/setup-draft.cql";
     private static final String DATA_DIRECTORY = "/src/main/resources/D8-data/";
 
     public static void main(String[] args) {
@@ -17,11 +17,9 @@ public class Test {
         Cluster cluster;
         Session session;
 
-        // Connect to the cluster and keyspace "demo"
+        // Connect to the cluster
         cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
         session = cluster.connect();
-
-        System.out.println();
 
         DbBuilder builder = new DbBuilder(System.getProperty("user.dir") + SCHEMA_FILE_PATH);
         try {
@@ -29,6 +27,13 @@ public class Test {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        DataLoader loader = new DataLoader(System.getProperty("user.dir") + DATA_DIRECTORY);
+//        try {
+//            loader.loadData(session);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         cluster.close();
     }
